f5da0e3db2c09376a1df20921998b2945c51b9d0
XAPHNE <suvnkr.xaphene@gmail.com>
2023-12-21 23:22:01 +0530

    Created layout footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+    <div id="footer">
+        <p>Copyright &copy; <a href="{{ url('/') }}">Logistics Aggregation System</a> 2023 - {{ date('Y') }} | All Rights Reserved</p>
+    </div>
+
+    <script src="{{ asset('js/custom.js') }}"></script>
+
+</body>
+</html>
